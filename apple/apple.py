0fc98d43c1f41a9bec9ad14003897e4f2f61df4f
Taeuk Gwak <gwak2837@kakao.com>
2022-01-12 15:53:39 +0900

    Update apple.py

--- apple/apple.py
+++ apple/apple.py
@@ -19,7 +19,28 @@ chat_ids = set(json.loads(os.getenv("CHAT_IDs")))
 
 print("Connect to the telegram bot...")
 telegram_bot = TelegramBot(apple_pickup_bot_token, chat_ids)
-telegram_bot.send_message("hello first message")
+
+URL = "https://www.apple.com/kr-k12/shop/bag/flyout?apikey=SJHJUH4YFCTTPD4F4&l=https%3A%2F%2Fwww.apple.com%2Fkr-k12%2Fshop%2Fgo%2Fhome"
+
+print("Fetching apple store...")
+while True:
+    try:
+        response = requests.get(URL)
+    except Exception as e:
+        print(e)
+
+    try:
+        text = response.json()["message"]["text"]
+        if text != "We'll be right back...":
+            telegram_bot.send_message(f"애플 스토어가 열렸어요, {datetime.datetime.now()}")
+            time.sleep(120)
+        else:
+            print(f"아직 애플 스토어 점검 중, {datetime.datetime.now()}")
+
+    except json.decoder.JSONDecodeError as e:
+        print(f"{e}, {datetime.datetime.now()}")
+
+    time.sleep(5)
 
 URL = "https://www.apple.com/kr-k12/shop/retail/pickup-message?pl=true&parts.0=MYFP2KH/A&location=06982"
 
@@ -30,13 +51,16 @@ while True:
     except Exception as e:
         print(e)
 
-    air4 = response.json()["body"]["stores"][0]["partsAvailability"]["MYFP2KH/A"]
-    message = f'{air4["storePickupQuote"]}, {air4["storePickupProductTitle"]}, {datetime.datetime.now()}'
+    try:
+        air4 = response.json()["body"]["stores"][0]["partsAvailability"]["MYFP2KH/A"]
+        message = f'{air4["storePickupQuote"]}, {air4["storePickupProductTitle"]}, {datetime.datetime.now()}'
 
-    if air4["storeSelectionEnabled"]:
-        telegram_bot.send_message(message)
-        time.sleep(3600)
-    else:
-        print(message)
+        if air4["storeSelectionEnabled"]:
+            telegram_bot.send_message(message)
+            time.sleep(3600)
+        else:
+            print(message)
+    except json.decoder.JSONDecodeError as e:
+        print(f"{e}, {datetime.datetime.now()}")
 
     time.sleep(10)
